841eea2c381fe6df7ed242631dd9b3d8bb7969b4
thismali <mali@mail.umkc.edu>
2015-09-15 08:31:26 -0500

    lab #2 using country info service api

---
+++ L2/ViewControllerResults.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerResults.h
+//  RestServiceDemoLab2
+//
+//  Created by Ali, Mohamoud (UMKC-Student) on 9/14/15.
+//  Copyright (c) 2015 Ali, Mohamoud (UMKC-Student). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewControllerResults : ViewController
+
+@end
